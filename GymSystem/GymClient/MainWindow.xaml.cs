01a2d8e0ba703b5e5b77e6e2efe9e39f4fdd587e
Michael <micaelpc@gmail.com>
2018-12-14 18:19:14 +0200

    client infra

--- GymSystem/GymClient/MainWindow.xaml.cs
+++ GymSystem/GymClient/MainWindow.xaml.cs
@@ -22,23 +22,67 @@ namespace GymClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        public ObservableCollection<Trainee> trainees = new ObservableCollection<Trainee>();
+
+
+
+
+        public string koko
+        {
+            get { return (string)GetValue(kokoProperty); }
+            set { SetValue(kokoProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for koko.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty kokoProperty =
+            DependencyProperty.Register("koko", typeof(string), typeof(MainWindow), new PropertyMetadata("ghfjhfjhgf"));
+
+
+
+        public UserControl CurrentPageViewModel
+        {
+            get { return (UserControl)GetValue(CurrentPageViewModelProperty); }
+            set { SetValue(CurrentPageViewModelProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for CurrentPageViewModel.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty CurrentPageViewModelProperty =
+            DependencyProperty.Register("CurrentPageViewModel", typeof(UserControl), typeof(MainWindow), new PropertyMetadata(null));
+
+
+
+
+        public ObservableCollection<Trainee> Trainees
+        {
+            get { return (ObservableCollection<Trainee>)GetValue(TraineesProperty); }
+            set { SetValue(TraineesProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Trainees.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TraineesProperty =
+            DependencyProperty.Register("Trainees", typeof(ObservableCollection<Trainee>),
+                typeof(MainWindow), new PropertyMetadata(new ObservableCollection<Trainee>()));
+
+
+
         public MainWindow()
         {
             InitializeComponent();
             InitTraineesMock();
+            CurrentPageViewModel = new UserControlTest();
+
         }
 
         private void InitTraineesMock()
         {
-            //trainees.Add(new Trainee
-            //      ("300951212", "מיכאל", "כהן", "לויתן 6 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
-            //trainees.Add(new Trainee
-            //     ("300952212", "טל", "כהן", "לויתן 5 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
-            //trainees.Add(new Trainee
-            //     ("300953212", "מיכאל", "שלטי", "לויתן 7 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
-            //trainees.Add(new Trainee
-            //     ("300954212", "טל", "שלטי", "לויתן 86 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
+
+            Trainees.Add(new Trainee
+                  ("300951212", "מיכאל", "כהן", "לויתן 6 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
+            Trainees.Add(new Trainee
+                 ("300952212", "טל", "כהן", "לויתן 5 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
+            Trainees.Add(new Trainee
+                 ("300953212", "מיכאל", "שלטי", "לויתן 7 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
+            Trainees.Add(new Trainee
+                 ("300954212", "טל", "שלטי", "לויתן 86 חולון", "0528998829", "0528998829", "micaelpc@gmail.com", DateTime.Now, "רגיש ללקטוז", null));
         }
 
     }
