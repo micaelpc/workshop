9b18dec91a2f4e228dd55598ea0cf7ee1d6d4384
Michael <micaelpc@gmail.com>
2018-12-23 19:26:29 +0200

    workdays

--- GymSystem/GymClient/TrainersUCs/TrainerUC.xaml.cs
+++ GymSystem/GymClient/TrainersUCs/TrainerUC.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using GymBL.Entities;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -18,11 +21,57 @@ namespace GymClient
     /// <summary>
     /// Interaction logic for TrainerUC.xaml
     /// </summary>
-    public partial class TrainerUC : UserControl
+    public partial class TrainerUC : UserControl, INotifyPropertyChanged
     {
         public TrainerUC()
         {
             InitializeComponent();
+            InitActiveTrainersList();
         }
+
+        private void InitActiveTrainersList()
+        {
+            //TODO TAL set List of all active trainerts (with prop is active 1 )
+            ///and replace that mock
+            ///in the active trainers
+            ActiveTrainers =  new ObservableCollection<Trainer> {
+                new Trainer {Firstname = "מאמני" ,Surname="מגניבי",IDNumber="123456789"},
+                new Trainer {Firstname = "מאמני1" ,Surname="מגניבי1",IDNumber="122456789"},
+                new Trainer {Firstname = "2מאמני" ,Surname="מגניבי2",IDNumber="124456789"}
+            };
+        }
+
+
+
+        private ObservableCollection<Trainer> _activeTrainers = new ObservableCollection<Trainer>();
+
+        public ObservableCollection<Trainer> ActiveTrainers {
+            get { return _activeTrainers; }
+            set {
+                _activeTrainers = value;
+                OnPropertyChanged("ActiveTrainers");
+            }
+
+        }
+
+
+
+        #region INotifyPropertyChanged Imp
+        public event PropertyChangedEventHandler PropertyChanged;
+        // Create the OnPropertyChanged method to raise the event
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+        #endregion
+
+
+
+
+
+
+
+
+
     }
 }
