04a7da75c7e20c5a4dda87e09342765459fcc82f
Shalti <talshalti1@gmail.com>
2018-12-28 17:08:06 +0200

    added reports and cleaned the solution

---
+++ GymSystem/GymGUI/GymBL/Report/NewSubscribersReport.cs
@@ -0,0 +1,19 @@
+﻿using GymBL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymBL.Report
+{
+    public class NewSubscribersReport
+    {
+        public NewSubscribersReport(DateTime since) {
+            var subs = Database.Database.GetInstance().GetAll<Subscription>($"StartT >= '{since.ToString("yyyy-MM-dd HH:mm:ss.fff")}'");
+            this.Trainees = subs.Select(x => x.Trainee).ToList();
+        }
+
+        public List<Trainee> Trainees { get; }
+    }
+}
---
+++ GymSystem/GymGUI/GymBL/Report/SubscriptionNearEnd.cs
@@ -0,0 +1,18 @@
+﻿using GymBL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GymBL.Report
+{
+    class SubscriptionNearEnd
+    {
+        public SubscriptionNearEnd(DateTime until)
+        {
+            var subs = Database.Database.GetInstance().GetAll<Subscription>($"EndT <= '{until.ToString("yyyy-MM-dd HH:mm:ss.fff")}' and isActive = 1");
+            this.Trainees = subs.Select(x => x.Trainee).ToList();
+        }
+
+        public List<Trainee> Trainees { get; }
+    }
+}
